f28702f7494431513f33289bde1c7b97b1440419
punkyoon <cryptography@konkuk.ac.kr>
2025-04-20 16:52:23 +0900

    add security config

---
+++ demo/src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package com.example.demo.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .sessionManagement { it.disable() }
+
+        return http.build()
+    }
+}
