b70b7326bba9a7dcc55f971fef3535bded3a3a95
punkyoon <cryptography@konkuk.ac.kr>
2025-04-20 17:14:29 +0900

    leave a log in `checkHealth` method

--- demo/src/main/kotlin/com/example/demo/health/HealthController.kt
+++ demo/src/main/kotlin/com/example/demo/health/HealthController.kt
@@ -1,5 +1,6 @@
 package com.example.demo.health
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController
 class HealthController {
     @GetMapping("/health")
     fun checkHealth(): HealthResponse {
+        logger.info("Checking health..")
+
         return HealthResponse(status = "OK")
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(HealthController::class.java)
     }
 }
