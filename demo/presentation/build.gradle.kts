58448aa390723de654f584aad9c5befbba30fdc7
punkyoon <cryptography@konkuk.ac.kr>
2025-04-22 00:24:17 +0900

    module updates

---
+++ demo/presentation/build.gradle.kts
@@ -0,0 +1,12 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.security:spring-security-test")
+
+    testImplementation(kotlin("test"))
+}
