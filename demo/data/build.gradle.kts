bf291a14b49c4e143579e9dbebcc1273a12ec101
punkyoon <cryptography@konkuk.ac.kr>
2025-04-28 02:03:38 +0900

    setup jpa and flyway

--- demo/data/build.gradle.kts
+++ demo/data/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     testImplementation(kotlin("test"))
 }
 
