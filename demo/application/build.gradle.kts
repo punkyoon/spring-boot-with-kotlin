5b62acf69478928b0a2523a335cabbcfcb83d735
punkyoon <cryptography@konkuk.ac.kr>
2025-05-07 03:10:22 +0900

    set jwt dependency

--- demo/application/build.gradle.kts
+++ demo/application/build.gradle.kts
@@ -2,6 +2,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("com.auth0:java-jwt:4.5.0")
+
     testImplementation(kotlin("test"))
 }
 
