b47aa87d6429312e68324ccc6d4d264b7de5c42c
punkyoon <cryptography@konkuk.ac.kr>
2025-05-07 03:09:04 +0900

    add request id filter

---
+++ demo/presentation/src/main/kotlin/com/example/demo/config/RequestIdFilter.kt
@@ -0,0 +1,23 @@
+package com.example.demo.config
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.util.UUID
+
+@Component
+class RequestIdFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val requestId = UUID.randomUUID().toString()
+
+        response.setHeader("X-Request-ID", requestId)
+
+        filterChain.doFilter(request, response)
+    }
+}
